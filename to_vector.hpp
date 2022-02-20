ae4344b5b770b82bb18407e020156b43f9989832
poti-san <100054092+poti-san@users.noreply.github.com>
2022-02-20 22:53:46 +0900

    Add files via upload

---
+++ to_vector.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <ranges>
+
+namespace ranges_util
+{
+	class to_vector_fn
+	{
+	public:
+		to_vector_fn() = default;
+
+		template <std::ranges::input_range Range>
+			requires std::same_as<std::ranges::iterator_t<Range>, std::ranges::sentinel_t<Range>>
+		constexpr auto operator()(Range&& r) const {
+			return std::vector<std::ranges::range_value_t<Range>>(std::ranges::begin(r), std::ranges::end(r));
+		}
+
+		template <std::ranges::input_range Range>
+		constexpr auto operator()(Range&& r) const {
+			std::vector<std::ranges::range_value_t<Range>> v;
+			std::ranges::copy(std::ranges::begin(r), std::ranges::end(r), std::back_inserter(v));
+			return std::move(v);
+		}
+
+
+		template <std::ranges::input_range Range>
+		constexpr friend auto operator |(Range&& r, to_vector_fn f) {
+			return f(r);
+		}
+	};
+	const to_vector_fn to_vector;
+
+	template <typename ElementT>
+	class to_vector_as_fn
+	{
+	public:
+		using element_type = ElementT;
+		to_vector_as_fn() = default;
+
+		template <std::ranges::input_range Range>
+			requires std::same_as<std::ranges::iterator_t<Range>, std::ranges::sentinel_t<Range>>
+		constexpr auto operator()(Range&& r) const {
+			return std::vector<element_type>(std::ranges::begin(r), std::ranges::end(r));
+		}
+
+		template <std::ranges::input_range Range>
+		constexpr auto operator()(Range&& r) const {
+			std::vector<element_type> v;
+			std::ranges::copy(std::ranges::begin(r), std::ranges::end(r), std::back_inserter(v));
+			return std::move(v);
+		}
+
+
+		template <typename ElementT, std::ranges::input_range Range>
+		constexpr friend auto operator |(Range&& r, to_vector_as_fn<ElementT> f) {
+			return f(r);
+		}
+	};
+	template <typename ElementT>
+	const to_vector_as_fn<ElementT> to_vector_as;
+}
---
+++ to_vector_test.cpp
@@ -0,0 +1,21 @@
+#include <array>
+#include <vector>
+
+#include "to_vector.hpp"
+
+int main()
+{
+	// std::views::iotaの仕様により無限ループになります。
+	// auto v1{ std::views::iota(0) | ranges_util::to_vector };
+
+	auto v2{ std::views::iota(0, 5) | ranges_util::to_vector };
+	// { 0, 1, 2, 3, 4 }
+
+	auto v3{ std::array{0, 1, 2, 3, 4} | ranges_util::to_vector };
+	// { 0, 1, 2, 3, 4 }
+
+	auto v4{ std::views::iota(0, 5) | ranges_util::to_vector_as<double> };
+	// { 0.0, 1.0, 2.0, 3.0, 4.0 }
+
+	return 0;
+}
